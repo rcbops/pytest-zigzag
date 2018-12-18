4ab1acbccc45bc88f0e8a11dc388aab1e2906c8a
mwbutcher <mark.butcher@rackspace.com>
2018-12-18 06:21:02 -0800

    bringing back _validate_qtest_token because its used on 169

--- pytest_zigzag/__init__.py
+++ pytest_zigzag/__init__.py
@@ -45,7 +45,6 @@ def _capture_marks(items, mark_names):
                 for arg in marker.args:
                     item.user_properties.append((marker.name, arg))
 
-
 def _capture_ci_environment(session):
     """Capture the CI environment variables for the current session using the scheme specified by the user.
 
@@ -104,9 +103,11 @@ def _get_option_of_highest_precedence(config, option_name):
         ini_option = None
 
     highest_precedence = cli_option or ini_option
-
     return highest_precedence
 
+
+def _validate_qtest_token(token):
+    return token if re.match("^[a-zA-Z0-9]+$", token) else ""
 
 def _load_config_file(config_file):
     """Validate and load the contents of a 'pytest-zigzag' config file into memory.
